e00c380f4bbfde81bab17550696d96118bbc3e19
CaptainRusio <enrique.aviles.m@usach.cl>
2020-03-29 00:21:37 -0300

    CHG: Se modifica modelo recipe

--- api/models/recipe.js
+++ api/models/recipe.js
@@ -1,7 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Recipe = sequelize.define('Recipe', {
-    name: DataTypes.STRING
+    name: DataTypes.STRING, 
+    preparation: DataTypes.STRING
   }, {});
   Recipe.associate = function(models) {
     // associations can be defined here
